135071035f60b0fed641937d9c01f60bd4793fdf
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2021-12-08 16:11:08 +0100

    added project folders

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const Color PRIMARY_COLOR=Color(0xffFCD224);
+  static const Color SECONDERY_COLOR=Color(0xff337BFF);
+}
---
+++ lib/constants/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+
+  static const String ADD_PDF="assets/images/addPdf.png";
+  static const String BOOKS="assets/images/books.png";
+  static const String COURSES="assets/images/course.png";
+  static const String EXAMS="assets/images/exams.png";
+  static const String ONINE_COURSE="assets/images/onlineCourse.png";
+  static const String PDF_FILE="assets/images/pdfFile.png";
+}
