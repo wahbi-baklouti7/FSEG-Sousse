ad422650c48e525c5bf3ef27e347150f31b2adb5
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2022-03-02 17:37:21 +0100

    make some change

--- lib/constants/app_size.dart
+++ lib/constants/app_size.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-class AppSize{
-
-  static  EdgeInsetsGeometry get APP_PADDING=>EdgeInsets.all(24);
+class AppSize {
+  static EdgeInsetsGeometry get APP_PADDING => const EdgeInsets.all(24);
 }
